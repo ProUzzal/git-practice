65a485c583ec41e3aaa94ee2c2ca5dd77bfff654
unknown <uzzalahmed0001@gmail.com>
2021-05-08 22:50:18 +0600

    Edited another two lines and edited filename

--- myJsFile.js
+++ myJsFile.js
@@ -2,7 +2,12 @@ function greetings(name) {
 
     console.log("Welcome to my github account", name);
 
+message(name);
+}
 
+function message(n){
+
+    console.log(n,"Use handwash and mask when you go outside");
 }
 
 greetings("Uzzal");
